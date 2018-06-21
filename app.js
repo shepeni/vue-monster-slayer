67da0be95dbf39477a183bfcc19f9318a5dfa552
Omar Rodriguez H <shepeni@gmail.com>
2018-06-20 21:11:33 -0500

    Funcionalidad para el funcionamiento del boton

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 new Vue({
-    
+    el: "#app",
+    data:{
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false
+    },
+    methods: {
+        startGame: function() {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        },
+        attack: function(){
+
+        },
+        specialAttack: function(){
+
+        },
+        heal: function(){
+            
+        },
+        giveUp: function(){
+            
+        }
+    }
 
 });
